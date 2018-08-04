0243a739b1667943aa9f91650cf3f16a5b5778e2
Tameez <etameez@gmail.com>
2018-08-04 13:19:36 +0200

    Added eigen matrix library

--- src/CPPMoMinterface/content/CPPMoMsolver/src/main.cpp
+++ src/CPPMoMinterface/content/CPPMoMsolver/src/main.cpp
@@ -14,13 +14,24 @@
 
 #include "mom_file_reader.h"
 #include "mom_solver.h"
+#include <Eigen/Dense>
+using Eigen::MatrixXd;
 
 int main()
 {
   // std::string path = "/home/tameez/Dropbox/pec_plate.mom";
-  std::string path = "C:\\Users\\Tameez\\Dropbox\\pec_plate.mom";
+  // std::string path = "C:\\Users\\Tameez\\Dropbox\\pec_plate.mom";
+  std::string path = "/Users/tameez/Dropbox/pec_plate.mom";
   MoMFileReader reader(path);
   MoMSolver solver(reader.getNodes(), reader.getTriangles(), reader.getEdges(), reader.getVrhs(), reader.getConstMap());
   solver.calculateZmnByFace();
+
+  // Eigen test
+  MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  std::cout << m << std::endl;
   return 0;
 }
