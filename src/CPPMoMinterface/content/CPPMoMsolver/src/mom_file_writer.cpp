5919023c974e9b2d8a9cecb0fce49ee2d4a01916
Tameez <etameez@gmail.com>
2018-08-09 15:31:34 +0200

    Initial class to write to file

---
+++ src/CPPMoMinterface/content/CPPMoMsolver/src/mom_file_writer.cpp
@@ -0,0 +1,13 @@
+#include "mom_file_writer.h"
+
+/**
+ *   \file mom_file_writer.cpp
+ *   \brief Write results to a file that will be read by MATLAB
+ *
+ *  Detailed description
+ *	A class to write Zmn, In and Vm to a file to be read by MATLAB 	
+ *
+ *  Author:  Tameez Ebrahim
+ *  Created: 09 August 2018 
+ *
+ */
---
+++ src/CPPMoMinterface/content/CPPMoMsolver/src/mom_file_writer.h
@@ -0,0 +1,10 @@
+#ifndef MOM_FILE_WRITER
+#define MOM_FILE_WRITER
+
+class MoMFileWriter
+{
+	public:
+		MoMFileWriter();
+};
+
+#endif
