a0eeae3624efb90281ed3f2e244d25c5b00c2922
Tameez <etameez@gmail.com>
2018-08-16 21:56:46 +0200

    Switched to setting OMP_NUM_THREADS to env variable

--- src/CPPMoMinterface/content/CPPMoMsolver/build_mpi.sh
+++ src/CPPMoMinterface/content/CPPMoMsolver/build_mpi.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 cd build_mpi/
-cmake -DMPI=ON .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake -DMPI=ON -DATLAS=ON .. -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 make
-mpiexec -np 4 --bind-to none ./mom_mpi
+time mpiexec -np 1 -genv OMP_NUM_THREADS=2 --bind-to none ./mom_mpi
+time mpiexec -np 2 -genv OMP_NUM_THREADS=2 --bind-to none ./mom_mpi
+time mpiexec -np 3 -genv OMP_NUM_THREADS=2 --bind-to none ./mom_mpi
+time mpiexec -np 4 -genv OMP_NUM_THREADS=2 --bind-to none ./mom_mpi
+time mpiexec -np 5 -genv OMP_NUM_THREADS=2 --bind-to none ./mom_mpi
+time mpiexec -np 6 -genv OMP_NUM_THREADS=2 --bind-to none ./mom_mpi
+time mpiexec -np 7 -genv OMP_NUM_THREADS=2 --bind-to none ./mom_mpi
+time mpiexec -np 8 -genv OMP_NUM_THREADS=2 --bind-to none ./mom_mpi
 
