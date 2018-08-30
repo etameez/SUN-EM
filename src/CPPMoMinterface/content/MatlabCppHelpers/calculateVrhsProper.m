fcad98653656d1feba2885ad217fa870f5dff20b
Tameez <etameez@gmail.com>
2018-08-30 14:18:53 +0200

    Working Vrhs for any linear plane wave

--- src/CPPMoMinterface/content/MatlabCppHelpers/calculateVrhsProper.m
+++ src/CPPMoMinterface/content/MatlabCppHelpers/calculateVrhsProper.m
@@ -1,11 +1,11 @@
-function [yVectors] = calculateVrhsProper(Const, Solver_setup)
+function [yVectors] = calculateVrhsProper(Const, Solver_setup, EMag, theta_0, phi_0, prop_direction)
     
     % For an incident plane wave
     % Only for x-directed plane wave
     freq = Solver_setup.frequencies.samples(1);
     k = 2 * pi / (Const.C0 / freq);
     %jk = [0 0 1i*k];
-    jk = 1i * k;
+    %jk = 1i * k;
     
     num_dofs = Solver_setup.num_metallic_edges;       % Replacing global NUM_DOFS
     ell = Solver_setup.rwg_basis_functions_length_m;  % Replacing global ELL
@@ -15,26 +15,37 @@ function [yVectors] = calculateVrhsProper(Const, Solver_setup)
     yVectors.values = zeros(num_dofs,1); % RHS matrix
     E_pls = zeros(num_dofs,3); % Sampled incident electric field
     E_mns = zeros(num_dofs,3);
-    % x-polarized field, unity magnitude
-    %E(:,1) = EMag; % Special case
-    %E(:,2) = 1; % Special case
+    
+    theta_rad = theta_0 * Const.DEG2RAD;
+    phi_rad = phi_0 * Const.DEG2RAD;
+    
+    if prop_direction == 0
+        Ex = cos(phi_rad) * cos(theta_rad);
+        Ey = sin(phi_rad) * cos(theta_rad);
+        Ez = -sin(theta_rad);
+        
+        if theta_rad == pi
+            Ez = 0;
+        end
+        
+        E = [Ex Ey Ez]; 
+    end
+    
+    kx = sin(theta_rad) * cos(phi_rad);
+    ky = sin(theta_rad) * sin(phi_rad);
+    kz = cos(theta_rad);
+    jk = [1i * k * kx 1i * k * ky 1i * k * kz];
     
     for m = 1:num_dofs
         t_pls = Solver_setup.rwg_basis_functions_trianglePlus(m);
         t_mns = Solver_setup.rwg_basis_functions_triangleMinus(m);
-        %E_pls(m) = exp(dot(jk, Solver_setup.triangle_centre_point(t_pls,:)));
-        %E_mns(m) = exp(dot(jk, Solver_setup.triangle_centre_point(t_mns,:)));
         
-        % MATLAB has a bug in their dot product
-        % If you do a dot product between a complex and real number, 
-        % it incorrectly uses the complex conjugate on the complex number
-        % when it should be taking the conjugate of the real number for no
-        % net change
-        E_pls(m) = exp(jk * Solver_setup.triangle_centre_point(t_pls, 3));
-        E_mns(m) = exp(jk * Solver_setup.triangle_centre_point(t_mns, 3));               
+        E_pls(m,:) = E.* exp(dot(Solver_setup.triangle_centre_point(t_pls,:), jk));
+        E_mns(m,:) = E.* exp(dot(Solver_setup.triangle_centre_point(t_mns,:), jk));               
     end
     
     for m = 1:num_dofs              
-        yVectors.values(m) = E_pls(m,1) * rho_c_pls(m,1)/2 + E_mns(m,1) * rho_c_mns(m,1)/2;
+        yVectors.values(m) = 0.5 * dot(rho_c_pls(m,:), E_pls(m,:)) + ...
+                             0.5 * dot(rho_c_mns(m,:), E_mns(m,:));
         yVectors.values(m) = ell(m)*yVectors.values(m);
     end
